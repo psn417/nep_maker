0c7cb27c970b4324f44c5101bd84620918f29ea0
psn417 <psn417@icloud.com>
2025-04-09 12:52:10 +0800

    update example

--- examples/Na/run_active.py
+++ examples/Na/run_active.py
@@ -9,16 +9,20 @@ from ase.io import read
 from nep_maker.active_learning import ActiveLearning
 
 
-def get_gpumd_input(P, T):
+def get_gpumd_input():
     input = f"""
-replicate   4 4 4
+replicate   5 5 5
 potential   ../nep.txt
-velocity    {T}
+velocity    250
 time_step   1
 
 compute_extrapolation asi_file ../active_set.asi check_interval 10 gamma_low 5 gamma_high 10
-ensemble npt_mttk iso {P} {P} temp {T} {T}
-run         100000
+ensemble npt_mttk iso 0 0 temp 250 500
+run         250000
+
+compute_extrapolation asi_file ../active_set.asi check_interval 10 gamma_low 5 gamma_high 10
+ensemble npt_mttk iso 0 0 temp 500 250
+run         250000
 """
     return input
 
@@ -27,6 +31,6 @@ atoms = read("Na.xyz")
 
 a = ActiveLearning()
 a.max_structures_per_iteration = 20
-a.input_gpumd = [get_gpumd_input(0, T) for T in range(300, 501, 50)]
-a.atoms_gpumd = [atoms]*len(a.input_gpumd )
+a.input_gpumd = [get_gpumd_input() for _ in range(8)]
+a.atoms_gpumd = [atoms]*len(a.input_gpumd)
 a.run()
